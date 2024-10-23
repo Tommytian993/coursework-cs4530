adb250cc0a332c3459236abb7758e7141e458c3c
Jerrywang993 <wang.tianye2@northeastern.edu>
2024-10-23 23:07:59 +0000

    feat(assignments): complete A1/A2/A3 list with details

--- src/Kanbas/Courses/Assignments/index.tsx
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -15,9 +15,34 @@ export default function Assignments() {
           >
             A1 - ENV + HTML
           </a>
+          <div>
+            Multiple Modules | <b>Not available until</b> May 6 at 12:00am |{" "}
+            <b>Due</b> May 13 at 11:59pm | 100 pts
+          </div>
         </li>
         <li className="wd-assignment-list-item">
-          {/* Complete On Your Own */}
+          <a
+            className="wd-assignment-link"
+            href="#/Kanbas/Courses/1234/Assignments/124"
+          >
+            A2 - CSS + BOOTSTRAP
+          </a>
+          <div>
+            Multiple Modules | <b>Not available until</b> May 13 at 12:00am |{" "}
+            <b>Due</b> May 20 at 11:59pm | 100 pts
+          </div>
+        </li>
+        <li className="wd-assignment-list-item">
+          <a
+            className="wd-assignment-link"
+            href="#/Kanbas/Courses/1234/Assignments/125"
+          >
+            A3 - JAVASCRIPT + REACT
+          </a>
+          <div>
+            Multiple Modules | <b>Not available until</b> May 20 at 12:00am |{" "}
+            <b>Due</b> May 27 at 11:59pm | 100 pts
+          </div>
         </li>
       </ul>
     </div>
