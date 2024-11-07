e51ca67ad0dfe64c5a43a810facfa9da775b1d1d
Jerrywang993 <wang.tianye2@northeastern.edu>
2024-11-07 17:25:12 +0000

    feat(account): style Sign In screen and Account Navigation with Bootstrap classes

--- src/Kanbas/Account/Signin.tsx
+++ src/Kanbas/Account/Signin.tsx
@@ -3,20 +3,30 @@ import { Link } from "react-router-dom";
 export default function Signin() {
   return (
     <div id="wd-signin-screen">
-      <h3>Sign in</h3>
-      <input className="wd-username" placeholder="username" /> <br />
+      <h1>Sign in</h1>
       <input
-        className="wd-password"
+        id="wd-username"
+        placeholder="username"
+        className="form-control mb-2"
+      />
+      <input
+        id="wd-password"
         placeholder="password"
         type="password"
-      />{" "}
-      <br />
-      <Link id="wd-signin-btn" to="/Kanbas/Dashboard">
-        {" "}
-        Sign in{" "}
-      </Link>{" "}
-      <br />
-      <Link id="wd-signup-link" to="/Kanbas/Account/Signup">
+        className="form-control mb-2"
+      />
+      <Link
+        id="wd-signin-btn"
+        to="/Kanbas/Dashboard"
+        className="btn btn-primary w-100 mb-2"
+      >
+        Sign in
+      </Link>
+      <Link
+        id="wd-signup-link"
+        to="/Kanbas/Account/Signup"
+        className="text-decoration-none"
+      >
         Sign up
       </Link>
     </div>
