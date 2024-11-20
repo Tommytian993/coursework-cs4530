b7ff73cbda8ce7c14fe884790a101db3b36b7018
Jerrywang993 <wang.tianye2@northeastern.edu>
2024-11-20 17:45:12 +0000

    feat(modules): implement delete module functionality with trash icon

--- src/Kanbas/Courses/Modules/ModuleControlButtons.tsx
+++ src/Kanbas/Courses/Modules/ModuleControlButtons.tsx
@@ -1,10 +1,21 @@
 import { FaPlus } from "react-icons/fa6";
+import { FaTrash } from "react-icons/fa";
 import { IoEllipsisVertical } from "react-icons/io5";
 import GreenCheckmark from "./GreenCheckmark";
 
-export default function ModuleControlButtons() {
+export default function ModuleControlButtons({
+  moduleId,
+  deleteModule,
+}: {
+  moduleId: string;
+  deleteModule: (moduleId: string) => void;
+}) {
   return (
     <div className="float-end">
+      <FaTrash
+        className="text-danger me-2 mb-1"
+        onClick={() => deleteModule(moduleId)}
+      />
       <GreenCheckmark />
       {FaPlus({ className: "me-2 fs-4" })}
       {IoEllipsisVertical({ className: "fs-4" })}
