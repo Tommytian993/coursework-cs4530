ce36d854e0f81611ab19ab59cec14c92d6ff8a07
Jerrywang993 <wang.tianye2@northeastern.edu>
2024-11-21 11:35:22 +0000

    feat(auth): implement ProtectedRoute to secure Dashboard and Courses routes

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -3,6 +3,7 @@ import Account from "./Account";
 import Dashboard from "./Dashboard";
 import KanbasNavigation from "./Navigation";
 import Courses from "./Courses";
+import ProtectedRoute from "./Account/ProtectedRoute";
 import "./styles.css";
 
 export default function Kanbas() {
@@ -11,10 +12,24 @@ export default function Kanbas() {
       <KanbasNavigation />
       <div className="wd-main-content-offset p-3">
         <Routes>
-          <Route path="/" element={<Navigate to="Account" />} />
+          <Route path="/" element={<Navigate to="Dashboard" />} />
           <Route path="/Account/*" element={<Account />} />
-          <Route path="/Dashboard" element={<Dashboard />} />
-          <Route path="/Courses/:cid/*" element={<Courses />} />
+          <Route
+            path="/Dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/Courses/:cid/*"
+            element={
+              <ProtectedRoute>
+                <Courses />
+              </ProtectedRoute>
+            }
+          />
           <Route path="/Calendar" element={<h1>Calendar</h1>} />
           <Route path="/Inbox" element={<h1>Inbox</h1>} />
         </Routes>
