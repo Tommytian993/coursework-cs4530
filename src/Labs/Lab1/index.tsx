16efc6cdd01f0fddc74fba4203c12cdf858160aa
Jerrywang993 <wang.tianye2@northeastern.edu>
2025-03-22 13:12:49 +0000

    feat(lab1): add List Tags section with ordered and unordered lists

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -41,6 +41,45 @@ export default function Lab1() {
           tag to tell browsers to render the gaps.
         </p>
       </div>
+      <div id="wd-lists">
+        <h4>List Tags</h4>
+        <h5>Ordered List Tag</h5>
+        How to make pancakes:
+        <ol id="wd-pancakes">
+          <li>Mix dry ingredients.</li>
+          <li>Add wet ingredients.</li>
+          <li>Stir to combine.</li>
+          <li>Heat a skillet or griddle.</li>
+          <li>Pour batter onto the skillet.</li>
+          <li>Cook until bubbly on top.</li>
+          <li>Flip and cook the other side.</li>
+          <li>Serve and enjoy!</li>
+        </ol>
+        My favorite recipe:
+        <ol id="wd-your-favorite-recipe">
+          <li>Boil water in a large pot</li>
+          <li>Add pasta and cook for 8-10 minutes</li>
+          <li>Drain pasta and return to pot</li>
+          <li>Add your favorite sauce and stir</li>
+          <li>Serve hot with grated cheese</li>
+        </ol>
+        <h5>Unordered List Tag</h5>
+        My favorite books (in no particular order)
+        <ul id="wd-my-books">
+          <li>Dune</li>
+          <li>Lord of the Rings</li>
+          <li>Ender's Game</li>
+          <li>Red Mars</li>
+          <li>The Forever War</li>
+        </ul>
+        Your favorite books (in no particular order)
+        <ul id="wd-your-books">
+          <li>The Hitchhiker's Guide to the Galaxy</li>
+          <li>1984</li>
+          <li>Brave New World</li>
+          <li>Fahrenheit 451</li>
+        </ul>
+      </div>
     </div>
   );
 }
