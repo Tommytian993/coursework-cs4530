616523ec4c3fd9752b367ad2fa39cd842b1ab733
Jerrywang993 <wang.tianye2@northeastern.edu>
2025-03-24 17:30:44 +0000

    feat(lab1): add Form Elements section with text fields

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -175,6 +175,22 @@ export default function Lab1() {
         <br />
         <img id="wd-teslabot" src="images/teslabot.jpg" height="200px" />
       </div>
+      <div id="wd-forms">
+        <h4>Form Elements</h4>
+        <form id="wd-text-fields">
+          <h5>Text Fields</h5>
+          <label htmlFor="wd-text-fields-username">Username:</label>
+          <input id="wd-text-fields-username" placeholder="jdoe" /> <br />
+          <label htmlFor="wd-text-fields-password">Password:</label>
+          <input type="password" id="wd-text-fields-password" value="123@#$asd" />
+          <br />
+          <label htmlFor="wd-text-fields-first-name">First name:</label>
+          <input type="text" id="wd-text-fields-first-name" title="John" /> <br />
+          <label htmlFor="wd-text-fields-last-name">Last name:</label>
+          <input type="text" id="wd-text-fields-last-name" placeholder="Doe"
+            value="Wonderland" title="The last name" />
+        </form>
+      </div>
     </div>
   );
 }
