90b6f73fc521e741a492e0964d3085381caa50e3
Jerrywang993 <wang.tianye2@northeastern.edu>
2025-03-28 14:33:21 +0000

    feat(lab2): add foreground and background color components

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -1,4 +1,6 @@
 import "./index.css";
+import ForegroundColors from "./ForegroundColors";
+import BackgroundColors from "./BackgroundColors";
 export default function Lab2() {
   return (
     <div id="wd-lab2">
@@ -26,30 +28,35 @@ export default function Lab2() {
           Instead of using IDs to refer to elements, you can use an element's
           CLASS attribute
         </p>
-                <h4 className="wd-class-selector">
-This heading has same style as paragraph above
+        <h4 className="wd-class-selector">
+          This heading has same style as paragraph above
         </h4>
       </div>
       <div id="wd-css-document-structure">
         <div className="wd-selector-1">
           <h3>Document structure selectors</h3>
           <div className="wd-selector-2">
-            Selectors can be combined to refer elements in particular
-            places in the document
+            Selectors can be combined to refer elements in particular places in
+            the document
             <p className="wd-selector-3">
               This paragraph's red background is referenced as
               <br />
-              .selector-2 .selector3<br />
-              meaning the descendant of some ancestor.<br />
+              .selector-2 .selector3
+              <br />
+              meaning the descendant of some ancestor.
+              <br />
               <span className="wd-selector-4">
                 Whereas this span is a direct child of its parent
-              </span><br />
-                You can combine these relationships to create specific 
-                styles depending on the document structure
+              </span>
+              <br />
+              You can combine these relationships to create specific styles
+              depending on the document structure
             </p>
           </div>
         </div>
       </div>
+      <ForegroundColors />
+      <BackgroundColors />
     </div>
   );
 }
