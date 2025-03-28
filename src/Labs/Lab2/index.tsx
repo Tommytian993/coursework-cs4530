c492f1df2abda1d1622c9f425224de4c269ec5e5
Jerrywang993 <wang.tianye2@northeastern.edu>
2025-03-28 23:17:42 +0000

    feat(lab2): add flex component with flexbox layout and grow properties

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -10,6 +10,7 @@ import Positions from "./Positions";
 import Zindex from "./Zindex";
 import Float from "./Float";
 import GridLayout from "./GridLayout";
+import Flex from "./Flex";
 export default function Lab2() {
   return (
     <div id="wd-lab2">
@@ -75,6 +76,7 @@ export default function Lab2() {
       <Zindex />
       <Float />
       <GridLayout />
+      <Flex />
     </div>
   );
 }
