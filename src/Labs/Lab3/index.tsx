dd29286e05a2f0e89d53f5e08ee3ddc5f00a01e3
Jerrywang993 <wang.tianye2@northeastern.edu>
2025-04-01 12:30:45 +0000

    feat(lab3): add VariablesAndConstants, VariableTypes, and BooleanVariables components

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,7 +1,14 @@
+import VariablesAndConstants from "./VariablesAndConstants";
+import VariableTypes from "./VariableTypes";
+import BooleanVariables from "./BooleanVariables";
+
 export default function Lab3() {
   return (
     <div id="wd-lab3">
       <h3>Lab 3</h3>
+      <VariablesAndConstants/>
+      <VariableTypes/>
+      <BooleanVariables/>
     </div>
   );
 }
